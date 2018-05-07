bb2f99e4e41a604ac6842e672c165946b4760ca0
Rochele Edenís <rochele.edenis@gmail.com>
2018-05-07 00:35:17 -0300

    Initial Database Setup With Seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,31 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function ($faker) {
+    return [
+        'title'   => $faker->sentence,
+        'body'    => $faker->paragraph,
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        }
+    ];
+});
+
+$factory->define(App\Reply::class, function ($faker) {
+    return [
+        'body'     => $faker->paragraph,
+        'user_id'  => function () {
+            return factory('App\User')->create()->id;
+        },
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        }
     ];
 });
